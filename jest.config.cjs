390375aba693bf8e332ce1370db54510350a5dfa
olivier <opattin@asd-int.com>
2025-08-18 10:07:02 +0200

    jest unit test & coverage installed

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,8 +1,13 @@
 module.exports = {
+  collectCoverageFrom: ['src/**/*.js', '!**/node_modules/**'],
+  coverageReporters: ['html', 'text', 'text-summary', 'cobertura'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
   testEnvironment: 'jest-environment-jsdom',
   transform: {
     '^.+\\.vue$': '@vue/vue3-jest',
     '^.+\\.js$': 'babel-jest',
   },
   moduleFileExtensions: ['vue', 'js', 'json', 'node'],
-};
+}
