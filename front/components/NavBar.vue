fc539f90bc3d50fc70625197ea29aa9119998426
olivier <opattin@asd-int.com>
2025-08-29 16:07:24 +0200

    navbar menu color

--- front/components/NavBar.vue
+++ front/components/NavBar.vue
@@ -1,10 +1,10 @@
 <template>
   <v-app-bar app color="grey-darken-4">
     <template v-if="securityStore.getIsAuthenticated">
-      <v-btn v-for="route in filteredRoutes" :key="route.name" text :title="route.meta?.title"
-        variant="flat" rounded="0" class="mr-1" color="primary">
+      <v-btn v-for="route in filteredRoutes" :key="route.name" text :title="route.meta?.title" variant="flat"
+        rounded="0" class="mr-1" color="primary">
         <template v-slot:prepend>
-          <FlagIcon v-if="route.meta.flag" :code="route.meta.flag" size="18"/>
+          <FlagIcon v-if="route.meta.flag" :code="route.meta.flag" size="18" />
           <v-icon v-else-if="route.meta.icon">{{ route.meta.icon }}</v-icon>
         </template>
         {{ $helpers.capitalizeFirstLetter($t(route.name)) }}
@@ -14,7 +14,8 @@
 
         <v-menu activator="parent" open-on-hover open-delay="10" close-delay="10" v-if="route.meta.isDropdown">
           <v-list>
-            <v-list-item v-for="child in getChildren(route)" :key="child.name" :to="child.path" density="compact" base-color="blue-darken-4">
+            <v-list-item v-for="child in getChildren(route)" :key="child.name" :to="child.path" density="compact"
+              base-color="blue-darken-4">
               <v-list-item-title>{{ $helpers.capitalizeFirstLetter($t(child.name)) }}</v-list-item-title>
               <template v-slot:prepend>
                 <FlagIcon v-if="child.meta.flag" :code="child.meta.flag" />
